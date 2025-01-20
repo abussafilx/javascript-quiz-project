a6ec3d24f412f42c2b9d8a6e1ab8783eff16bfbb
Victor Abussafi <victorabussafi@gmail.com>
2025-01-20 16:18:18 +0000

    day 1 done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,47 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
 
-    // 2. getQuestion()
     
-    // 3. moveToNextQuestion()
+    constructor (questions, timeLimit, timeRemaining){
+      this.questions = questions; 
+      this.timeLimit = timeLimit;
+      this.timeRemaining = timeRemaining;
+      this.correctAnswers = 0;
+      this.currentQuestionIndex = 0;
+    };
 
-    // 4. shuffleQuestions()
+    getQuestion(){
 
-    // 5. checkAnswer(answer)
+    return this.questions[this.currentQuestionIndex];
 
-    // 6. hasEnded()
+    };
+    
+    moveToNextQuestion(){
+        this.currentQuestionIndex++;
+    };
+
+
+     shuffleQuestions(){
+
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            const randomIndex = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[randomIndex]] = [this.questions[randomIndex], this.questions[i]];
+        }
+
+     };
+
+    checkAnswer(answer){
+     if(this.questions[this.currentQuestionIndex].answer === answer){
+     this.correctAnswers++;
+     }
+    };
+
+   hasEnded(){
+
+   if(this.currentQuestionIndex < this.questions.length){
+    return false;
+
+   }else if(this.currentQuestionIndex === this.questions.length){
+      return true;
+   }
+    };
 }
